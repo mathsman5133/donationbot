3ac7007decae75f4b36d4ec428ed353472d7de6b
mathsman5133 <mathsman5132@gmail.com>
2019-06-25 21:50:09 +1000

    Add a command to invite the bot to your server

---
+++ cogs/info.py
@@ -0,0 +1,17 @@
+from discord.ext import commands
+import discord
+
+
+class Info(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(aliases=['join'])
+    async def invite(self, ctx):
+        """Get an invite to add the bot to your server.
+        """
+        await ctx.send(f'<{discord.utils.oauth_url(self.bot.client_id)}>')
+
+    @commands.group()
+    async def info(self, ctx):
+        pass
