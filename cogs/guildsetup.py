f9080c36867152bbce9ec7f42ea5c8e0a8c06818
mathsman5133 <mathsman5132@gmail.com>
2019-07-01 14:20:43 +1000

    Buggys galore

--- cogs/guildsetup.py
+++ cogs/guildsetup.py
@@ -178,7 +178,7 @@ class GuildConfiguration(commands.Cog):
 
         data.append(f"Clans: {', '.join('{n[0]} ({n[1})' for n in fetch)}")
 
-        message = await cog.get_message(guild_config.updates_header_id)
+        message = await cog.get_message(message_id=guild_config.updates_header_id)
         timestamp = message.embeds[0].timestamp
         if timestamp:
             data.append(f"Last Updated: {timestamp:%Y-%m-%d %H:%M:%S%z}")
