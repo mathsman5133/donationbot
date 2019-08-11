2ea2e51f8cfc58ae4dfc79f7bb79e3d3f96df17d
mathsman5133 <mathsman5132@gmail.com>
2019-08-11 17:43:17 +1000

    utilise a custom lru cache class for coc.py

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
 
 coc_client = coc.login(creds.email, creds.password, client=coc.EventsClient,
-                       key_names='test', throttle_limit=40, default_cache_class=LRU)
+                       key_names='test', throttle_limit=40)
 
 initial_extensions = (
     'cogs.guildsetup',
