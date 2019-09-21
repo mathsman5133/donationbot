5552e4cf700a4e610f67d80df9186db6cfb07c4c
W. Patrick Jones <wpmjones@gmail.com>
2019-09-21 17:27:57 -0400

    updates to DateConverter regex

--- cogs/utils/converters.py
+++ cogs/utils/converters.py
@@ -75,92 +75,62 @@ class ClanConverter(commands.Converter):
 
 class DateConverter(commands.Converter):
     """Convert user input into standard format date (YYYY-MM-DD)"""
-    async def convert(self, ctx, argument):
-        year_options = (f'{datetime.today().year}|{datetime.today().year+1}|'
-                        f'{str(datetime.today().year)[2:]}|{str(datetime.today().year+1)[2:]}')
+
+    async def convert(self, ctx, *, argument):
+        error_msg = 'You may think that\'s a date, but I don\'t. Try using the DD-MM-YYYY format.'
+        year_options = (f'{datetime.today().year}|{datetime.today().year + 1}|'
+                        f'{str(datetime.today().year)[2:]}|{str(datetime.today().year + 1)[2:]}')
 
         # Check for text based month with day first
-        pattern = (r'(?P<Date>\d+)[\s ]+'
+        pattern = (r'(?P<Date>\d{1,2})[/.\- ]'
                    r'(?P<Month>Jan(uary)?|Feb(ruary)?|Mar(ch)?|Apr(il)?|May|Jun(e)?|'
-                   r'Jul(y)?|Aug(ust)?|Sep(tember)?|Sept|Oct(ober)?|Nov(ember)?|Dec(ember)?)[\s ]+'
+                   r'Jul(y)?|Aug(ust)?|Sep(tember)?|Sept|Oct(ober)?|Nov(ember)?|Dec(ember)?)[/.\- ]'
                    r'(?P<Year>' + year_options + ')')
         match = re.match(pattern, argument, re.IGNORECASE)
         if match:
             date_string = f"{match.group('Year')} {match.group('Month')[:3]} {match.group('Date')}"
-            try:
-                if len(match.group('Year')) == 2:
-                    return datetime.strptime(date_string, '%y %b %d')
-                else:
-                    return datetime.strptime(date_string, '%Y %b %d')
-            except ValueError:
-                raise commands.BadArgument(
-                    'You may think that\'s a date, but I don\'t. Try using the YYYY-MM-DD format.')
-
-        # Check for text based month with month first (optional comma)
-        pattern = (r'(?P<Month>Jan(uary)?|Feb(ruary)?|Mar(ch)?|Apr(il)?|May|Jun(e)?|'
-                   r'Jul(y)?|Aug(ust)?|Sep(tember)?|Sept|Oct(ober)?|Nov(ember)?|Dec(ember)?)[\s ]?'
-                   r'(?P<Date>\d{1,2}),?[\s ]?'
-                   r'(?P<Year>' + year_options + ')')
-        match = re.match(pattern, argument, re.IGNORECASE)
-        if match:
-            date_string = f"{match.group('Year')} {match.group('Month')[:3]} {match.group('Date')}"
-            try:
-                if len(match.group('Year')) == 2:
-                    return datetime.strptime(date_string, '%y %b %d')
-                else:
-                    return datetime.strptime(date_string, '%Y %b %d')
-            except ValueError:
-                raise commands.BadArgument(
-                    'You may think that\'s a date, but I don\'t. Try using the YYYY-MM-DD format.')
-
-        # Check for text based month with month last
-        pattern = (r'(?P<Year>' + year_options + r')[\s ]+'
-                   r'(?P<Date>\d+),?[\s ]+'
-                   r'(?P<Month>Jan(uary)?|Feb(ruary)?|Mar(ch)?|Apr(il)?|May|Jun(e)?|'
-                   r'Jul(y)?|Aug(ust)?|Sep(tember)?|Sept|Oct(ober)?|Nov(ember)?|Dec(ember)?)')
-        match = re.match(pattern, argument, re.IGNORECASE)
-        if match:
-            date_string = f"{match.group('Year')} {match.group('Month')[:3]} {match.group('Date')}"
-            try:
-                if len(match.group('Year')) == 2:
-                    return datetime.strptime(date_string, '%y %b %d')
-                else:
-                    return datetime.strptime(date_string, '%Y %b %d')
-            except ValueError:
-                raise commands.BadArgument(
-                    'You may think that\'s a date, but I don\'t. Try using the YYYY-MM-DD format.')
-
-        # Check for dates with year at the end
-        pattern = r'(\d{1,2})[/ -.]?(\d{1,2})[/ -.]?(?P<Year>' + year_options + ')'
-        match = re.match(pattern, argument, re.IGNORECASE)
-        if match:
-            if match.group(1) == match.group(2):
-                date = month = match.group(1)
-            elif int(match.group(1)) > 12:
-                date = match.group(1)
-                month = match.group(2)
+            if len(match.group('Year')) == 2:
+                fmt = '%y %b %d'
             else:
-                month = match.group(1)
-                date = match.group(2)
-            year = match.group('Year')
+                fmt = '%Y %b %d'
 
-        # Check for dates with year at the beginning (then assume MM-DD)
-        pattern = r'(?P<Year>' + year_options + r')[/ -.]?(?P<Month>\d{1,2})[/ -.]?(?P<Date>\d{1,2})'
+        # Check for text based month with month first
+        pattern = (r'(?P<Month>Jan(uary)?|Feb(ruary)?|Mar(ch)?|Apr(il)?|May|Jun(e)?|'
+                   r'Jul(y)?|Aug(ust)?|Sep(tember)?|Sept|Oct(ober)?|Nov(ember)?|Dec(ember)?)[/.\- ]'
+                   r'(?P<Date>\d{1,2})[/.\- ]'
+                   r'(?P<Year>' + year_options + ')')
         match = re.match(pattern, argument, re.IGNORECASE)
         if match:
-            date = match.group('Date')
-            month = match.group('Month')
-            year = match.group('Year')
+            date_string = f"{match.group('Year')} {match.group('Month')[:3]} {match.group('Date')}"
+            if len(match.group('Year')) == 2:
+                fmt = '%y %b %d'
+            else:
+                fmt = '%Y %b %d'
+
+        # Check for YYYY-MM-DD
+        pattern = (r'(?P<Year>' + year_options + r')[/.\- ](?P<Month>\d{1,2})[/.\- ](?P<Date>\d{1,2})')
+        match = re.match(pattern, argument, re.IGNORECASE)
+        if match:
+            date_string = f"{match.group('Year')} {match.group('Month')} {match.group('Date')}"
+            if len(match.group('Year')) == 2:
+                fmt = '%y %m %d'
+            else:
+                fmt = '%Y %m %d'
+
+        # Check for DD-MM-(YY)YY
+        pattern = (r'(?P<Date>\d{1,2})[/.\- ](?P<Month>\d{1,2})[/.\- ](?P<Year>' + year_options + ')')
+        match = re.match(pattern, argument, re.IGNORECASE)
+        if match:
+            date_string = f"{match.group('Year')} {match.group('Month')} {match.group('Date')}"
+            if len(match.group('Year')) == 2:
+                fmt = '%y %m %d'
+            else:
+                fmt = '%Y %m %d'
 
         try:
-            date_string = f"{year} {month} {date}"
-            if len(year) == 2:
-                return datetime.strptime(date_string, '%y %m %d')
-            else:
-                return datetime.strptime(date_string, '%Y %m %d')
+            return datetime.strptime(date_string, fmt)
         except (ValueError, NameError):
-            raise commands.BadArgument(
-                'You may think that\'s a date, but I don\'t. Try using the YYYY-MM-DD format.')
+            raise commands.BadArgument(error_msg)
 
 
 class GlobalChannel(commands.Converter):
