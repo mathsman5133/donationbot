966b3b2d2eae76c2094221e374ccfe0a5bd42175
mathsman5133 <mathsman5132@gmail.com>
2019-09-21 11:11:47 +1000

    create a lightweight cached call to get channel id of guilds trophy/donation boards and use that for a non-cached call to get config for a guild id and board type combo

--- cogs/botutils.py
+++ cogs/botutils.py
@@ -12,11 +12,12 @@ class Utils(commands.Cog):
         self.bot = bot
 
     @cache()
-    async def log_config(self, channel_id, log_type) -> Union[LogConfig, None]:
+    async def log_config(self, channel_id: int, log_type: str) -> Union[LogConfig, None]:
         query = """SELECT guild_id, 
                           channel_id, 
                           interval, 
-                          toggle 
+                          toggle,
+                          type 
                    FROM logs 
                    WHERE channel_id=$1 
                    AND type=$2
@@ -27,7 +28,7 @@ class Utils(commands.Cog):
 
         return LogConfig(bot=self.bot, record=fetch)
 
-    def invalidate_channel_configs(self, channel_id):
+    def invalidate_channel_configs(self, channel_id: int):
         self.log_config.invalidate(self, channel_id)
         # todo: fix
         task = self.bot.donationlogs._tasks.pop(channel_id, None)
@@ -35,7 +36,7 @@ class Utils(commands.Cog):
             task.cancel()
 
     @cache()
-    async def board_config(self, channel_id) -> Union[BoardConfig, None]:
+    async def board_config(self, channel_id: int) -> Union[BoardConfig, None]:
         query = """SELECT guild_id, 
                           channel_id,
                           icon_url,
@@ -54,28 +55,28 @@ class Utils(commands.Cog):
 
         return BoardConfig(bot=self.bot, record=fetch)
 
-    async def get_board_config(self, guild_id, board_type):
-        query = """SELECT guild_id, 
-                          channel_id,
-                          icon_url,
-                          title,
-                          render,
-                          toggle,
-                          type,
-                          in_event
-                   FROM boards 
-                   WHERE guild_id = $1
-                   AND type = $2
-                """
+    @cache(maxsize=40)
+    async def get_board_channel(self, guild_id: int, board_type: str) -> Union[int, None]:
+        query = "SELECT channel_id FROM boards WHERE guild_id = $1 AND type = $2;"
         fetch = await self.bot.pool.fetchrow(query, guild_id, board_type)
+        if fetch:
+            return fetch['channel_id']
 
-        if not fetch:
-            return None
+    async def get_board_config(self, guild_id: int, board_type: str, invalidate=False):
+        if invalidate:
+            self.get_board_channel.invalidate(self, guild_id, board_type)
 
-        return BoardConfig(bot=self.bot, record=fetch)
+        channel_id = await self.get_board_channel(guild_id, board_type)
+        if not channel_id:
+            return
+
+        if invalidate:
+            self.board_config.invalidate(self, channel_id)
+
+        return await self.board_config(channel_id)
 
     @cache()
-    async def event_config(self, guild_id) -> Union[SlimEventConfig, None]:
+    async def event_config(self, guild_id: int) -> Union[SlimEventConfig, None]:
         query = """SELECT id,
                           start,
                           finish,
@@ -92,7 +93,7 @@ class Utils(commands.Cog):
         return SlimEventConfig(fetch['id'], fetch['start'], fetch['finish'], fetch['event_name'])
 
     @cache()
-    async def get_clan_name(self, guild_id, tag) -> str:
+    async def get_clan_name(self, guild_id: int, tag: str) -> str:
         query = "SELECT clan_name FROM clans WHERE clan_tag=$1 AND guild_id=$2"
         fetch = await self.bot.pool.fetchrow(query, tag, guild_id)
         if not fetch:
@@ -100,7 +101,7 @@ class Utils(commands.Cog):
         return fetch[0]
 
     @cache()
-    async def get_message(self, channel, message_id) -> Union[discord.Message, None]:
+    async def get_message(self, channel: discord.TextChannel, message_id: int) -> Union[discord.Message, None]:
         try:
             o = discord.Object(id=message_id + 1)
             # don't wanna use get_message due to poor rate limit (1/1s) vs (50/1s)
