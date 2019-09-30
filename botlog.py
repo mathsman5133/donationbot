15a6af9b0fff26c5c7860ade939691b498344f6e
mathsman5133 <mathsman5132@gmail.com>
2019-09-30 17:47:09 +1000

    bugfix `+seasondump`

--- botlog.py
+++ botlog.py
@@ -16,7 +16,7 @@ def setup_logging(bot):
     handler = logging.FileHandler(filename='donationtracker.log', encoding='utf-8', mode='w')
     handler.setLevel(logging.INFO)
     stream_handler = logging.StreamHandler()
-    stream_handler.setLevel(logging.DEBUG)
+    stream_handler.setLevel(logging.INFO)
     dt_fmt = '%d-%m-%Y %H:%M:%S'
     fmt = logging.Formatter('[{asctime}] [{levelname:<7}] {name}: {message}', dt_fmt, style='{')
     handler.setFormatter(fmt)
