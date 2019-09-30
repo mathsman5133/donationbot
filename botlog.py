1b4937ebc9303b59e9dd9b73e2a2ea78646552e8
mathsman5133 <mathsman5132@gmail.com>
2019-09-30 14:00:11 +1000

    production settings

--- botlog.py
+++ botlog.py
@@ -8,15 +8,15 @@ def setup_logging(bot):
     logging.getLogger('discord').setLevel(logging.INFO)
     logging.getLogger('discord.http').setLevel(logging.WARNING)
     logging.getLogger('websockets.protocol').setLevel(logging.WARNING)
-    logging.getLogger('coc').setLevel(logging.DEBUG)
+    logging.getLogger('coc').setLevel(logging.INFO)
     logging.getLogger('coc.http').setLevel(logging.WARNING)
 
     log = logging.getLogger()
-    log.setLevel(logging.DEBUG)
+    log.setLevel(logging.INFO)
     handler = logging.FileHandler(filename='donationtracker.log', encoding='utf-8', mode='w')
     handler.setLevel(logging.INFO)
     stream_handler = logging.StreamHandler()
-    stream_handler.setLevel(logging.DEBUG)
+    stream_handler.setLevel(logging.WARNING)
     dt_fmt = '%d-%m-%Y %H:%M:%S'
     fmt = logging.Formatter('[{asctime}] [{levelname:<7}] {name}: {message}', dt_fmt, style='{')
     handler.setFormatter(fmt)
