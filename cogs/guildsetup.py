ebafa35b8331916e25602bbff00e1dcbf3e5a3ad
W. Patrick Jones <wpmjones@gmail.com>
2019-10-03 21:29:50 -0400

    had on/off backwards - sorry

--- cogs/guildsetup.py
+++ cogs/guildsetup.py
@@ -1390,9 +1390,9 @@ class GuildConfiguration(commands.Cog, name='Server Setup'):
                 """
         toggle = await ctx.db.execute(query, ctx.config.channel_id, 'donation')
         if toggle:
-            condition = 'off'
-        else:
             condition = 'on'
+        else:
+            condition = 'off'
         await ctx.send(f'Logs for {ctx.config.channel.mention} have been turned {condition}.')
 
     @edit.group(name='trophylog')
@@ -1461,9 +1461,9 @@ class GuildConfiguration(commands.Cog, name='Server Setup'):
                 """
         toggle = await ctx.db.execute(query, ctx.config.channel_id, 'trophy')
         if toggle:
-            condition = 'off'
-        else:
             condition = 'on'
+        else:
+            condition = 'off'
         await ctx.send(f'Logs for {ctx.config.channel.mention} have been turned {condition}.')
 
     @edit.command(name='event')
