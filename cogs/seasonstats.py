1f1c20cb7e24c9a5e879a78d76a8e39fab0362e3
mathsman5133 <mathsman5132@gmail.com>
2019-10-04 21:12:38 +1000

    get seasons commands working again

--- cogs/seasonstats.py
+++ cogs/seasonstats.py
@@ -11,10 +11,12 @@ from cogs.boards import MockPlayer
 from cogs.utils.emoji_lookup import number_emojis
 from cogs.utils.error_handler import error_handler
 from cogs.utils.paginator import SeasonStatsPaginator
-from cogs.utils.formatters import TabularData, readable_time, CLYTable
-from cogs.utils.cache import cache
+from cogs.utils.formatters import TabularData, readable_time, CLYTable, get_render_type
+from cogs.utils.cache import cache, Strategy
 from cogs.utils.db_objects import DatabasePlayer
 
+
+mock = MockPlayer('Unknown', 'Unknown')
 
 class Season(commands.Cog):
     def __init__(self, bot):
@@ -23,7 +25,7 @@ class Season(commands.Cog):
         self.season_stats_guild_entries = {}
         self.season_stats_user_entries = {}
 
-    @cache()
+    @cache(strategy=Strategy.lru)
     async def build_season_clan_misc_stats(self, ctx, clans, season_id):
         clan_tags = [n.tag for n in clans]
 
@@ -34,7 +36,7 @@ class Season(commands.Cog):
                           MIN(time),
                           SUM(donations),
                           SUM(received)
-                   FROM events 
+                   FROM donationevents 
                    WHERE clan_tag=ANY($1::TEXT[])
                    AND season_id=$2
                 """
@@ -61,7 +63,7 @@ class Season(commands.Cog):
                     value=value)
         return e
 
-    @cache()
+    @cache(strategy=Strategy.lru)
     async def build_season_clan_event_stats(self, ctx, clans, season_id):
         clan_tags = [n.tag for n in clans]
 
@@ -69,10 +71,10 @@ class Season(commands.Cog):
                           title=f'Season Event Stats for {ctx.guild.name}')
         query = """SELECT clans.clan_name,
                           COUNT(*) AS "uses"
-                   FROM events
+                   FROM donationevents
                    INNER JOIN clans
-                        ON events.clan_tag=clans.clan_tag
-                   WHERE events.clan_tag=ANY($1::TEXT[])
+                        ON donationevents.clan_tag=clans.clan_tag
+                   WHERE donationevents.clan_tag=ANY($1::TEXT[])
                    AND season_id=$2
                    GROUP BY clans.clan_name
                    ORDER BY "uses" DESC
@@ -89,11 +91,11 @@ class Season(commands.Cog):
 
         query = """SELECT clans.clan_name,
                           SUM(donations) AS "donations"
-                   FROM events
+                   FROM donationevents
                    INNER JOIN clans
-                        ON events.clan_tag=clans.clan_tag
-                   WHERE events.clan_tag=ANY($1::TEXT[])
-                   AND events.season_id=$2
+                        ON donationevents.clan_tag=clans.clan_tag
+                   WHERE donationevents.clan_tag=ANY($1::TEXT[])
+                   AND donationevents.season_id=$2
                    GROUP BY clans.clan_name
                    ORDER BY "donations" DESC
                    LIMIT 5;
@@ -108,11 +110,11 @@ class Season(commands.Cog):
 
         query = """SELECT clans.clan_name,
                           SUM(received) AS "received"
-                   FROM events
+                   FROM donationevents
                    INNER JOIN clans
-                        ON events.clan_tag=clans.clan_tag
-                   WHERE events.clan_tag=ANY($1::TEXT[])
-                   AND events.season_id=$2
+                        ON donationevents.clan_tag=clans.clan_tag
+                   WHERE donationevents.clan_tag=ANY($1::TEXT[])
+                   AND donationevents.season_id=$2
                    GROUP BY clans.clan_name
                    ORDER BY "received" DESC
                    LIMIT 5;
@@ -127,7 +129,7 @@ class Season(commands.Cog):
 
         query = """SELECT player_name,
                           COUNT(*) AS "uses"
-                   FROM events
+                   FROM donationevents
                    WHERE clan_tag=ANY($1::TEXT[])
                    AND season_id=$2
                    AND player_name IS NOT NULL
@@ -146,7 +148,7 @@ class Season(commands.Cog):
         e.set_footer(text='Page 2/3').timestamp = datetime.utcnow()
         return e
 
-    @cache()
+    @cache(strategy=Strategy.lru)
     async def build_season_clan_player_stats(self, ctx, clans, season_id):
         clan_tags = [n.tag for n in clans]
 
@@ -154,7 +156,7 @@ class Season(commands.Cog):
                           title=f'Season Player Stats for {ctx.guild.name}')
         query = """SELECT player_name,
                           COUNT(*) AS "uses"
-                   FROM events
+                   FROM donationevents
                    WHERE clan_tag=ANY($1::TEXT[])
                    AND season_id=$2
                    AND player_name IS NOT NULL
@@ -171,16 +173,16 @@ class Season(commands.Cog):
                     inline=False
                     )
 
-        query = """SELECT events.player_name,
+        query = """SELECT donationevents.player_name,
                           SUM(DISTINCT players.donations) as "donations"
                    FROM players
-                   INNER JOIN events 
-                        ON events.player_tag=players.player_tag
-                        AND events.season_id=players.season_id
-                   WHERE events.clan_tag=ANY($1::TEXT[])
-                   AND events.season_id=$2
-                   AND events.player_name IS NOT NULL
-                   GROUP BY events.player_name
+                   INNER JOIN donationevents 
+                        ON donationevents.player_tag=players.player_tag
+                        AND donationevents.season_id=players.season_id
+                   WHERE donationevents.clan_tag=ANY($1::TEXT[])
+                   AND donationevents.season_id=$2
+                   AND donationevents.player_name IS NOT NULL
+                   GROUP BY donationevents.player_name
                    ORDER BY "donations" DESC
                    LIMIT 5;
                 """
@@ -192,16 +194,16 @@ class Season(commands.Cog):
                     inline=False
                     )
 
-        query = """SELECT events.player_name,
+        query = """SELECT donationevents.player_name,
                           SUM(DISTINCT players.received) as "received"
                    FROM players
-                   INNER JOIN events 
-                        ON events.player_tag=players.player_tag
-                        AND events.season_id=players.season_id
-                   WHERE events.clan_tag=ANY($1::TEXT[])
-                   AND events.season_id=$2
-                   AND events.player_name IS NOT NULL
-                   GROUP BY events.player_name
+                   INNER JOIN donationevents 
+                        ON donationevents.player_tag=players.player_tag
+                        AND donationevents.season_id=players.season_id
+                   WHERE donationevents.clan_tag=ANY($1::TEXT[])
+                   AND donationevents.season_id=$2
+                   AND donationevents.player_name IS NOT NULL
+                   GROUP BY donationevents.player_name
                    ORDER BY "received" DESC
                    LIMIT 5;
                 """
@@ -215,21 +217,21 @@ class Season(commands.Cog):
         e.set_footer(text='Page 3/3').timestamp = datetime.utcnow()
         return e
 
-    @cache()
+    @cache(strategy=Strategy.lru)
     async def build_season_user_misc_stats(self, ctx, user, season_id):
         e = discord.Embed(colour=self.bot.colour,
                           title=f'Season Overview for {user}')
 
         query = """SELECT COUNT(*), 
-                          MIN(events.time),
+                          MIN(donationevents.time),
                           SUM(players.donations),
                           SUM(players.received)
-                   FROM events
+                   FROM donationevents
                    INNER JOIN players
-                        ON events.player_tag=players.player_tag
-                        AND events.season_id=players.season_id
+                        ON donationevents.player_tag=players.player_tag
+                        AND donationevents.season_id=players.season_id
                    WHERE players.user_id=$1
-                   AND events.season_id=$2
+                   AND donationevents.season_id=$2
                 """
         count = await ctx.db.fetchrow(query, user.id, season_id)
 
@@ -254,18 +256,18 @@ class Season(commands.Cog):
                     value=value)
         return e
 
-    @cache()
+    @cache(strategy=Strategy.lru)
     async def build_season_user_player_stats(self, ctx, user, season_id):
         e = discord.Embed(colour=self.bot.colour,
                           title=f'Season Player Stats for {user}')
-        query = """SELECT events.player_name,
+        query = """SELECT donationevents.player_name,
                           COUNT(*) AS "uses"
-                   FROM events
+                   FROM donationevents
                    INNER JOIN players
-                        ON players.player_tag=events.player_tag
-                        AND players.season_id=events.season_id
+                        ON players.player_tag=donationevents.player_tag
+                        AND players.season_id=donationevents.season_id
                    WHERE players.user_id=$1
-                   AND events.season_id=$2
+                   AND donationevents.season_id=$2
                    AND player_name IS NOT NULL
                    GROUP BY player_name
                    ORDER BY "uses" DESC
@@ -280,16 +282,16 @@ class Season(commands.Cog):
                     inline=False
                     )
 
-        query = """SELECT events.player_name,
+        query = """SELECT donationevents.player_name,
                           SUM(DISTINCT players.donations) as "donations"
                    FROM players
-                   INNER JOIN events 
-                        ON events.player_tag=players.player_tag
-                        AND events.season_id=players.season_id
+                   INNER JOIN donationevents 
+                        ON donationevents.player_tag=players.player_tag
+                        AND donationevents.season_id=players.season_id
                    WHERE players.user_id=$1
-                   AND events.season_id=$2
-                   AND events.player_name IS NOT NULL
-                   GROUP BY events.player_name
+                   AND donationevents.season_id=$2
+                   AND donationevents.player_name IS NOT NULL
+                   GROUP BY donationevents.player_name
                    ORDER BY "donations" DESC
                    LIMIT 5;
                 """
@@ -301,16 +303,16 @@ class Season(commands.Cog):
                     inline=False
                     )
 
-        query = """SELECT events.player_name,
+        query = """SELECT donationevents.player_name,
                           SUM(DISTINCT players.received) as "received"
                    FROM players
-                   INNER JOIN events 
-                        ON events.player_tag=players.player_tag
-                        AND events.season_id=players.season_id
+                   INNER JOIN donationevents 
+                        ON donationevents.player_tag=players.player_tag
+                        AND donationevents.season_id=players.season_id
                    WHERE players.user_id=$1
-                   AND events.season_id=$2
-                   AND events.player_name IS NOT NULL
-                   GROUP BY events.player_name
+                   AND donationevents.season_id=$2
+                   AND donationevents.player_name IS NOT NULL
+                   GROUP BY donationevents.player_name
                    ORDER BY "received" DESC
                    LIMIT 5;
                 """
@@ -324,63 +326,56 @@ class Season(commands.Cog):
         e.set_footer(text='Page 2/2').timestamp = datetime.utcnow()
         return e
 
-    @cache()
-    async def get_donationboard(self, guild_id, season_id):
-        query = "SELECT DISTINCT clan_tag FROM clans WHERE guild_id=$1"
-        fetch = await self.bot.pool.fetch(query, guild_id)
-        clans = await self.bot.coc.get_clans((n[0] for n in fetch)).flatten()
-        guild_config = await self.bot.donationboard.get_guild_config(guild_id)
+    @cache(strategy=Strategy.lru)
+    async def get_board_fmt(self, guild_id, season_id, board_type):
+        board_config = await self.bot.utils.get_board_config(guild_id, board_type)
+        clans = await self.bot.get_clans(guild_id)
 
         players = []
         for n in clans:
             players.extend(p for p in n.itermembers)
 
-        query = """SELECT player_tag, donations, received
-                    FROM players 
-                    WHERE player_tag=ANY($1::TEXT[])
-                    AND season_id=$2
-                    ORDER BY donations DESC
-                    LIMIT 100;
-                """
-        fetch = await self.bot.pool.fetch(query, [n.tag for n in players], season_id)
-        if not fetch:
+        top_players = await self.bot.donationboard.get_top_players(players, board_type, False)
+
+        if not top_players:
             e = discord.Embed(colour=self.bot.colour,
                               title='No Donations Found')
             return [e]
-        db_players = [DatabasePlayer(bot=self.bot, record=n) for n in fetch]
-        players = {n.tag: n for n in players if n.tag in set(x.player_tag for x in db_players)}
 
-        message_count = math.ceil(len(db_players) / 20)
+        players = {n.tag: n for n in players if n.tag in set(x['player_tag'] for x in top_players)}
+
+        message_count = math.ceil(len(top_players) / 20)
 
         embeds = []
         for i in range(message_count):
-            player_data = db_players[i * 20:(i + 1) * 20]
+            player_data = top_players[i*20:(i+1)*20]
             table = CLYTable()
 
             for x, y in enumerate(player_data):
-                index = i * 20 + x
-                if guild_config.donationboard_render == 2:
+                index = i*20 + x
+                if board_config.render == 2:
                     table.add_row([index,
-                                   y.donations,
-                                   players.get(y.player_tag, MockPlayer()).name])
+                                   y[1],
+                                   players.get(y['player_tag'], mock).name])
                 else:
                     table.add_row([index,
-                                   y.donations,
-                                   y.received,
-                                   players.get(y.player_tag, MockPlayer()).name
-                                   ]
-                                  )
+                                   y[1],
+                                   y[2],
+                                   players.get(y['player_tag'], mock).name])
 
-            fmt = table.render_option_2() if \
-                guild_config.donationboard_render == 2 else table.render_option_1()
-            e = discord.Embed(colour=self.bot.colour,
+            render = get_render_type(board_config, table)
+            fmt = render()
+
+            e = discord.Embed(colour=self.bot.donationboard.get_colour(board_type, False),
                               description=fmt,
-                              timestamp=datetime.utcnow())
-            e.set_author(name=guild_config.donationboard_title or 'DonationBoard',
-                         icon_url=guild_config.icon_url or 'https://cdn.discordapp.com/'
-                                                           'emojis/592028799768592405.png?v=1')
+                              timestamp=datetime.utcnow()
+                              )
+            e.set_author(name=board_config.title,
+                         icon_url=board_config.icon_url or 'https://cdn.discordapp.com/'
+                                                     'emojis/592028799768592405.png?v=1')
             e.set_footer(text=f'Historical DonationBoard; Season {season_id} - Page {i+1}/{message_count}')
             embeds.append(e)
+
         return embeds
 
     @commands.group(invoke_without_subcommand=True)
@@ -425,11 +420,8 @@ class Season(commands.Cog):
 
         • `+season donationboard 1` - donationboard for season 1
         """
-        return await ctx.send('This command is not available at the moment. Sorry!')
-
-        query = "SELECT COUNT(*) FROM events WHERE clan_tag=ANY($1::TEXT[]) AND"
-        embeds = await self.get_donationboard(ctx.guild.id, season
-                                              or (await self.bot.seasonconfig.get_season_id()) - 1)
+        embeds = await self.get_board_fmt(ctx.guild.id, season or (await self.bot.seasonconfig.get_season_id()) - 1,
+                                          'donation')
         p = SeasonStatsPaginator(ctx, entries=embeds)
         await p.paginate()
 
@@ -454,8 +446,6 @@ class Season(commands.Cog):
         • `+season stats #CLAN_TAG` - season stats for previous season for that clan tag.
         • `+season stats 4 @user` - season stats for season 4 for @user.
         """
-        return await ctx.send('This command is not available at the moment. Sorry!')
-
         if not arg:
             arg = await ctx.get_clans()
         if not season:
