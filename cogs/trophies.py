1822a912d36a29671f97f011730186b11da735cd
W. Patrick Jones <wpmjones@gmail.com>
2019-10-08 17:43:56 -0400

    remove trophy attacks (moving to seasonstats.py)

--- cogs/trophies.py
+++ cogs/trophies.py
@@ -183,53 +183,6 @@ class Trophies(commands.Cog):
 
         await p.paginate()
 
-    @trophies.command(name='attacks', hidden=True)
-    @requires_config('event', invalidate=True)
-    async def trophies_attacks(self, ctx):
-        """Get attack wins for all clans.
-
-           By default, you shouldn't need to call these sub-commands as the bot will
-           parse your argument and direct it to the correct sub-command automatically.
-
-           **Example**
-           :white_check_mark: `+trophies attacks`
-        """
-        in_event = ctx.config.start < datetime.utcnow() < ctx.config.finish
-        if in_event:
-            query = """SELECT player_tag, end_attacks - start_attacks as attacks, trophies, user_id 
-                        FROM eventplayers p 
-                        WHERE event_id = $1
-                        ORDER BY attacks DESC
-                    """
-            fetch = await ctx.db.fetch(query, ctx.config.id)
-
-            title = f"Attack wins for {ctx.config.event_name}"
-        else:
-            tags = []
-            clans = await ctx.get_clans()
-            for n in clans:
-                tags.extend(x.tag for x in n.itermembers)
-            query = """SELECT player_tag, end_attacks - start_attacks as attacks, trophies, user_id 
-                        FROM players
-                        WHERE player_tag=ANY($1::TEXT[])
-                        AND season_id=$2
-                        ORDER BY attacks DESC
-                    """
-            fetch = await ctx.db.fetch(query, tags, await self.bot.seasonconfig.get_season_id())
-
-            if not fetch:
-                return await ctx.send(f"No players claimed for clans "
-                                      f"`{', '.join(f'{c.name} ({c.tag})' for c in clans)}`"
-                                      )
-
-            title = f"Attack wins for {', '.join(f'{c.name}' for c in clans)}"
-
-        page_count = math.ceil(len(fetch) / 20)
-        ctx.config.render = 3
-        p = formatters.TrophyPaginator(ctx, data=fetch, title=title, page_count=page_count)
-
-        await p.paginate()
-
 
 def setup(bot):
     bot.add_cog(Trophies(bot))
