bcff7b9d06627094e5d5fbcbfb757d5d1c05665c
mathsman5133 <38833292+mathsman5133@users.noreply.github.com>
2020-02-14 22:49:18 +1100

    Update syncer.py

--- syncer.py
+++ syncer.py
@@ -374,10 +374,10 @@ async def on_clan_member_join(member, clan):
                                     start_best_trophies,
                                     start_update
                                     ) 
-                VALUES ($1,$2,$3,$4,$4,$5,$6 - $2,$7,$8,$9,$10,True) 
+                VALUES ($1,$2,$3,$4,$4,$5,$6::integer - $2::integer,$7,$8,$9,$10,True) 
                 ON CONFLICT (player_tag, season_id) 
                 DO UPDATE SET 
-                donations = $6 - start_friend_in_need,
+                donations = $6::integer - start_friend_in_need,
                 trophies = $4
             """
 
