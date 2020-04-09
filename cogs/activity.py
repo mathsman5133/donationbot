b6ade2559d09945298944619978c526aa3696747
mathsman5133 <mathsman5132@gmail.com>
2020-04-10 09:52:10 +1000

    swap around order

--- cogs/activity.py
+++ cogs/activity.py
@@ -111,7 +111,7 @@ class Activity(commands.Cog):
         name, fetch = data
         trophy_events = {}
         #{n[0]: n[1] for n in await ctx.db.fetch(query, clan[0].tag)}
-        donation_events = {n[0]: n[1] for n in fetch}
+        donation_events = {n[1]: n[0] for n in fetch}
 
         if not (trophy_events or donation_events):
             return await ctx.send(f"Not enough history. Please try again later.")
