26c6d33ad1ebec4cb7cff631fef0d022c88565ff
mathsman5133 <mathsman5132@gmail.com>
2020-04-10 15:49:22 +1000

    comment out the create for now

--- cogs/utils/converters.py
+++ cogs/utils/converters.py
@@ -262,48 +262,48 @@ class ActivityBarConverter(commands.Converter):
                 else:
                     raise commands.BadArgument("I tried to parse your argument as a channel, server, clan name, clan tag, player name or tag and couldn't find a match!")
 
-        query = "SELECT clans.id FROM activity_query INNER JOIN clans ON clans.clan_tag = activity_query.clan_tag WHERE clans.guild_id = $1"
-        fetch = await ctx.db.fetchrow(query, ctx.guild.id)
-        if not fetch:
-            await ctx.send("Loading clan activity values. This will take a minute. Please be patient.")
-            query = """
-                    WITH cte AS (
-                        SELECT player_tag, 
-                               donationevents.clan_tag, 
-                               date_trunc('HOUR', "time") AS "timer", 
-                               date_part('HOUR', "time") AS "hour", 
-                               COUNT(*) AS "counter"
-                        FROM donationevents 
-                        INNER JOIN clans ON clans.clan_tag = donationevents.clan_tag
-                        WHERE clans.guild_id = $1
-                        GROUP BY timer, player_tag, donationevents.clan_tag, hour
-                    ),  
-                    cte2 AS (
-                        SELECT player_tag, 
-                               trophyevents.clan_tag, 
-                               date_trunc('HOUR', "time") AS "timer", 
-                               date_part('HOUR', "time") AS "hour", 
-                               COUNT(*) AS "counter"
-                        FROM trophyevents 
-                        INNER JOIN clans ON clans.clan_tag = trophyevents.clan_tag
-                        WHERE clans.guild_id = $1
-                        AND trophyevents.league_id > 29000022
-                        AND trophyevents.trophy_change > 0
-                        GROUP BY timer, player_tag, trophyevents.clan_tag, "hour"                 
-                    )
-                    INSERT INTO activity_query (player_tag, clan_tag, hour_time, counter, hour_digit)
-                    SELECT cte.player_tag,
-                           cte.clan_tag,
-                           cte.timer,
-                           COALESCE(cte.counter, 0) + COALESCE(cte2.counter, 0),
-                           cte.hour
-                    FROM cte 
-                    FULL JOIN cte2 
-                    ON cte.player_tag = cte2.player_tag 
-                    AND cte.clan_tag = cte2.clan_tag 
-                    AND cte.timer = cte2.timer
-                    """
-            await ctx.db.execute(query, ctx.guild.id)
+        # query = "SELECT clans.id FROM activity_query INNER JOIN clans ON clans.clan_tag = activity_query.clan_tag WHERE clans.guild_id = $1"
+        # fetch = await ctx.db.fetchrow(query, ctx.guild.id)
+        # if not fetch:
+        #     await ctx.send("Loading clan activity values. This will take a minute. Please be patient.")
+        #     query = """
+        #             WITH cte AS (
+        #                 SELECT player_tag,
+        #                        donationevents.clan_tag,
+        #                        date_trunc('HOUR', "time") AS "timer",
+        #                        date_part('HOUR', "time") AS "hour",
+        #                        COUNT(*) AS "counter"
+        #                 FROM donationevents
+        #                 INNER JOIN clans ON clans.clan_tag = donationevents.clan_tag
+        #                 WHERE clans.guild_id = $1
+        #                 GROUP BY timer, player_tag, donationevents.clan_tag, hour
+        #             ),
+        #             cte2 AS (
+        #                 SELECT player_tag,
+        #                        trophyevents.clan_tag,
+        #                        date_trunc('HOUR', "time") AS "timer",
+        #                        date_part('HOUR', "time") AS "hour",
+        #                        COUNT(*) AS "counter"
+        #                 FROM trophyevents
+        #                 INNER JOIN clans ON clans.clan_tag = trophyevents.clan_tag
+        #                 WHERE clans.guild_id = $1
+        #                 AND trophyevents.league_id > 29000022
+        #                 AND trophyevents.trophy_change > 0
+        #                 GROUP BY timer, player_tag, trophyevents.clan_tag, "hour"
+        #             )
+        #             INSERT INTO activity_query (player_tag, clan_tag, hour_time, counter, hour_digit)
+        #             SELECT cte.player_tag,
+        #                    cte.clan_tag,
+        #                    cte.timer,
+        #                    COALESCE(cte.counter, 0) + COALESCE(cte2.counter, 0),
+        #                    cte.hour
+        #             FROM cte
+        #             FULL JOIN cte2
+        #             ON cte.player_tag = cte2.player_tag
+        #             AND cte.clan_tag = cte2.clan_tag
+        #             AND cte.timer = cte2.timer
+        #             """
+        #     await ctx.db.execute(query, ctx.guild.id)
 
         if guild:
             query = """
