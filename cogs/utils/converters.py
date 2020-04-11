01920ff54177baec7cc4b5e6e07b11a89e62dc21
mathsman5133 <mathsman5132@gmail.com>
2020-04-11 17:40:26 +1000

    debug

--- cogs/utils/converters.py
+++ cogs/utils/converters.py
@@ -351,14 +351,14 @@ class ActivityBarConverter(commands.Converter):
                         SELECT generate_series(min(hour_time), max(hour_time), '1 hour'::interval) as "time"
                         FROM activity_query 
                         WHERE player_tag = $1
-                        AND activity_query.hour_time > now() - '$2 days'::interval
+                        AND activity_query.hour_time > now() - ($2 ||' days')::interval
                     ),
                     
                     actual_times AS (
                         SELECT hour_time as "time", counter
                         FROM activity_query
                         WHERE player_tag = $1
-                        AND activity_query.hour_time > now() - '$2 days'::interval
+                        AND activity_query.hour_time > now() - ($2 ||' days')::interval
                     )
                     SELECT date_part('HOUR', valid_times."time") AS "hour", AVG(COALESCE(actual_times.counter, 0)), min(valid_times."time")
                     FROM valid_times
@@ -378,7 +378,7 @@ class ActivityBarConverter(commands.Converter):
                                DATE(activity_query.hour_time) as "date" 
                         FROM activity_query 
                         WHERE clan_tag = $1 
-                        AND activity_query.hour_time > (now() - '$2 days'::interval)
+                        AND activity_query.hour_time > now() - ($2 ||' days')::interval
                         GROUP by date
                     ),
                     cte2 AS (
@@ -388,7 +388,7 @@ class ActivityBarConverter(commands.Converter):
                         JOIN cte1 
                         ON cte1.date = date(hour_time) 
                         WHERE clan_tag = $1 
-                        AND activity_query.hour_time > (now() - '$2 days'::interval)
+                        AND activity_query.hour_time > now() - ($2 ||' days')::interval
                         GROUP BY hour_time
                     )
                     SELECT date_part('HOUR', hour_time) as "hour_digit", AVG(num_events), MIN(hour_time) 
