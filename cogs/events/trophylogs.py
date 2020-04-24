b1bec92ee1844a2cde0ab4466cd04128702268ba
mathsman5133 <mathsman5132@gmail.com>
2020-04-25 09:28:19 +1000

    fix `+edit trophylog toggle`, and tidy up edit commands.

--- cogs/events/trophylogs.py
+++ cogs/events/trophylogs.py
@@ -182,7 +182,7 @@ class TrophyLogs(commands.Cog):
             messages = []
             for x in events:
                 slim_event = SlimTrophyEvent(x['trophy_change'], x['league_id'], x['player_name'], x['clan_tag'])
-                clan_name = await self.bot.utils.get_clan_name(config.guild.id, x['clan_tag'])
+                clan_name = await self.bot.utils.get_clan_name(config.guild_id, x['clan_tag'])
                 messages.append(format_trophy_log_message(slim_event, clan_name))
 
             group_batch = []
