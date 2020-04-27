e2b66ed71b265f27ff756c9f4731917e04ca10b7
mathsman5133 <mathsman5132@gmail.com>
2020-04-27 16:58:00 +1000

    revert

--- reset_season.py
+++ reset_season.py
@@ -12,7 +12,7 @@ log = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 loop = asyncio.get_event_loop()
-client = coc.login(creds.email, creds.password, key_names='test2', key_count=6, throttle_limit=30)
+client = coc.login(creds.email, creds.password, key_names='test', key_count=3, throttle_limit=30)
 pool = loop.run_until_complete(Table.create_pool(creds.postgres))
 
 SEASON_ID = 11
@@ -23,18 +23,11 @@ for _ in range(10):
     except:
         pass
 
-async def new_season_pull():
-    s = time.perf_counter()
-    query = "SELECT DISTINCT player_tag FROM players WHERE season_id = $1 AND start_update = False;"
-    fetch = await pool.fetch(query, SEASON_ID)
 
-    for i in range(int(len(fetch) / 2000)):
-        await get_and_do_updates((n[0] for n in fetch[i:i+2000]), SEASON_ID)
+async def new_season_pull(number=5000):
+    query = "SELECT DISTINCT player_tag FROM players WHERE season_id = $1 AND start_update = False LIMIT $2;"
+    fetch = await pool.fetch(query, SEASON_ID, number)
 
-    log.critical(f"new season pull done, took {(time.perf_counter() - s)*1000}ms")
-
-async def get_and_do_updates(player_tags, season_id):
-    s = time.perf_counter()
     query = """UPDATE players SET start_friend_in_need = x.friend_in_need, 
                                   start_sharing_is_caring = x.sharing_is_caring,
                                   start_attacks = x.attacks,
@@ -43,7 +36,6 @@ async def get_and_do_updates(player_tags, season_id):
                                   start_best_trophies = x.best_trophies,
                                   start_update = True,
                                   ignore = TRUE
-
                 FROM(
                     SELECT x.player_tag, 
                            x.friend_in_need, 
@@ -52,7 +44,6 @@ async def get_and_do_updates(player_tags, season_id):
                            x.defenses,
                            x.trophies,
                            x.best_trophies
-
                     FROM jsonb_to_recordset($1::jsonb)
                     AS x(
                         player_tag TEXT, 
@@ -75,7 +66,6 @@ async def get_and_do_updates(player_tags, season_id):
                                   end_best_trophies = x.best_trophies,
                                   final_update = True,
                                   ignore = TRUE
-
                 FROM(
                     SELECT x.player_tag, 
                            x.friend_in_need, 
@@ -84,7 +74,6 @@ async def get_and_do_updates(player_tags, season_id):
                            x.defenses,
                            x.trophies,
                            x.best_trophies
-
                     FROM jsonb_to_recordset($1::jsonb)
                     AS x(
                         player_tag TEXT, 
@@ -100,8 +89,19 @@ async def get_and_do_updates(player_tags, season_id):
             WHERE players.player_tag = x.player_tag
             AND players.season_id=$2"""
 
+    counter = 0
     data = []
-    async for player in client.get_players(player_tags, cache=False, update_cache=False):
+    async for player in client.get_players((n[0] for n in fetch), cache=False, update_cache=False):
+        if counter == 100:
+            # This is basically to ensure we don't have 10k records in memory at any one time.
+            # Safety net incase something fails, too.
+            q = await pool.execute(query, data, SEASON_ID)
+            q2 = await pool.execute(query2, data, SEASON_ID - 1)
+            data.clear()
+            counter = 0
+            log.info(f"Done query: {q}, {q2}, {time.time()}")
+
+
         data.append({
             'player_tag': player.tag,
             'friend_in_need': player.achievements_dict['Friend in Need'].value,
@@ -111,10 +111,11 @@ async def get_and_do_updates(player_tags, season_id):
             'trophies': player.trophies,
             'best_trophies': player.best_trophies
         })
-
-    q = await pool.execute(query, data, season_id)
-    q2 = await pool.execute(query2, data, season_id - 1)
-    log.info(f"Done update players: {q}, {q2}, {(time.perf_counter() - s)*1000}ms")
+        counter += 1
 
 
-loop.run_until_complete(new_season_pull())
+for _ in range(int(sys.argv[1])):
+    try:
+        loop.run_until_complete(new_season_pull(1000))
+    except:
+        continue
