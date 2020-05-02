1e28aa0f5fa8a1137f7820772964ae474db61465
mathsman5133 <mathsman5132@gmail.com>
2020-05-02 14:52:24 +1000

    batch the query

--- syncer.py
+++ syncer.py
@@ -500,19 +500,19 @@ async def update_db():
                 SELECT DISTINCT clan_tag, activity_sync FROM clans INNER JOIN guilds ON clans.guild_id = guilds.guild_id
              )
              INSERT INTO activity_query (player_tag, clan_tag, counter, hour_digit)
-             SELECT x.player_tag, x.clan_tag, 1, date_part('HOUR', now())
+             SELECT x.player_tag, x.clan_tag, x.counter, date_part('HOUR', now())
              FROM jsonb_to_recordset($1::jsonb)
-             AS x(player_tag TEXT, clan_tag TEXT)
+             AS x(player_tag TEXT, clan_tag TEXT, counter INTEGER)
              INNER JOIN cte ON cte.clan_tag = x.clan_tag
              WHERE cte.activity_sync = TRUE
              ON CONFLICT (player_tag, clan_tag, hour_time)
-             DO UPDATE SET counter = activity_query.counter + 1
+             DO UPDATE SET counter = activity_query.counter + excluded.counter
              """
     async with last_updated_batch_lock:
         await pool.execute(
             query, list(last_updated_tags), SEASON_ID
         )
-        nice = [{"player_tag": player_tag, "clan_tag": clan_tag} for (player_tag, clan_tag) in last_updated_counter.elements()]
+        nice = [{"player_tag": player_tag, "clan_tag": clan_tag, "counter": counter} for ((player_tag, clan_tag), counter) in last_updated_counter.most_common()]
         await pool.execute(query2, nice)
         last_updated_tags.clear()
         last_updated_counter.clear()
