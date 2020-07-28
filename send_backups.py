eed9bb5a71700d4987875d90af776a7eb0c257ac
mathsman5133 <mathsman5132@gmail.com>
2020-07-28 21:52:32 +1000

    backups

--- send_backups.py
+++ send_backups.py
@@ -8,6 +8,6 @@ webhook = discord.Webhook.from_url(backups_url, adapter=discord.RequestsWebhookA
 
 for fp in os.listdir("/home/mathsman/backups2"):
     try:
-        webhook.send(file=discord.File(fp))
+        webhook.send(file=discord.File("/home/mathsman/backups2/" + fp))
     except Exception as e:
         print(e)
