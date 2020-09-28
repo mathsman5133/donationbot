39b80e434bcd005499f605b45ba867a7650798af
mathsman5133 <mathsman5132@gmail.com>
2020-09-28 16:47:05 +1000

    newseason

--- syncer.py
+++ syncer.py
@@ -45,7 +45,7 @@ class CustomClan(coc.Clan):
         self._members = {m['tag']: CustomClanMember(data=m, client=client) for m in data.get("memberList", [])}
 
 
-SEASON_ID = 15
+SEASON_ID = 16
 
 loop = asyncio.get_event_loop()
 pool = loop.run_until_complete(Table.create_pool(creds.postgres))
