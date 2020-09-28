148318b312f9bae332fbf3b79926609596c46ca7
mathsman5133 <mathsman5132@gmail.com>
2020-09-28 20:04:24 +1000

    improve look

--- cogs/utils/opencv_boards.py
+++ cogs/utils/opencv_boards.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import discord
 import io
 
@@ -22,18 +23,24 @@ class MPLTable:
         plt.figure(linewidth=2,
                    edgecolor='steelblue',
                    facecolor='skyblue',
-                   tight_layout={'pad': 1},
-                   # figsize=(5,3)
+                   #tight_layout={'pad': 1},
+                   figsize=(5,3)
                    )
-        row_headers = [str(i) + ". " + x.pop(0) for i, x in enumerate(self.rows, start=self.start)]
+        row_headers = [str(i) + ". " for i in range(self.start, len(self.rows) + self.start)]
+        rcolors = plt.cm.BuPu(np.full(len(row_headers), 0.1))
+        ccolors = plt.cm.BuPu(np.full(len(self.columns), 0.1))
+
         table = plt.table(
             cellText=self.rows,
             rowLabels=row_headers,
             rowLoc='right',
+            rowColours=rcolors,
+            colColours=ccolors,
             colLabels=self.columns,
             loc='center'
         )
-        table.scale(1, 2)
+
+        table.scale(0.8, 2)
 
         # Hide axes
         ax = plt.gca()
@@ -47,7 +54,13 @@ class MPLTable:
         # plt.figtext(0.95, 0.05, datetime, horizontalalignment='right', size=6, weight='light')
 
         b = io.BytesIO()
-        plt.savefig(b, format='png')
+        fig = plt.gcf()
+        plt.savefig(b,
+                    # bbox='tight',
+                    edgecolor=fig.get_edgecolor(),
+                    facecolor=fig.get_facecolor(),
+                    dpi=150
+                    )
         b.seek(0)
         plt.close()
         return b
@@ -55,11 +68,11 @@ class MPLTable:
 
 class DonationBoardTable(MPLTable):
     def __init__(self, board_name, start):
-        columns = ("Name", "Cups", "Gain", "Last On")
+        columns = ("Name", "Dons", "Rec", "Ratio", "Last On", )
         super().__init__(board_name, columns, start)
 
 
 class TrophyBoardTable(MPLTable):
     def __init__(self, board_name, start):
-        columns = ("Name", "Dons", "Rec", "Ratio", "Last On", )
+        columns = ("Name", "Cups", "Gain", "Last On")
         super().__init__(board_name, columns, start)
