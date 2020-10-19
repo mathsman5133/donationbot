5995b1e346783989c53e1ff33037b4ff924abb9e
mathsman5133 <mathsman5132@gmail.com>
2020-10-19 19:05:21 +1100

    rollback

--- cogs/admin.py
+++ cogs/admin.py
@@ -64,14 +64,14 @@ table {
   border-collapse: seperate;
   border-spacing: 0 12px;
   width: 100%;
-  padding-bottom: 35px;
-  padding-left: 35px;
-  padding-right: 35px
+  padding-bottom: 30px;
+  padding-left: 30px;
+  padding-right: 30px
 }
 
 td, th {
   text-align: left;
-  font-size: 45px;
+  font-size: 40px;
   padding: 10px;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 }
