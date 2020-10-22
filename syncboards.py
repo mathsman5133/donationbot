aa56a39d73de3e87862474bdb08f016d7f7ba34a
mathsman5133 <mathsman5132@gmail.com>
2020-10-22 14:07:03 +1100

    try to trap this bug

--- syncboards.py
+++ syncboards.py
@@ -50,17 +50,13 @@ class HTMLImages:
         else:
             self.columns = ("#", "Player Name", "Cups", "Gain", "Last On")
 
-        try:
-            if sort_by and donationboard:
-                sort_columns = ("#", "Player Name", "donations", "received", "ratio", "last_online ASC, player_name")
-                self.selected_index = [1, sort_columns.index(sort_by.replace('donation', 'donations'))]
-            elif sort_by:
-                sort_columns = ("#", "Player Name", "trophies", "gain", "last_online ASC, player_name")
-                self.selected_index = [1, sort_columns.index(sort_by.replace('donations', 'trophies'))]
-            else:
-                self.selected_index = []
-        except ValueError:
-            log.exception(sort_by)
+        if sort_by and donationboard:
+            sort_columns = ("#", "Player Name", "donations", "received", "ratio", "last_online ASC, player_name")
+            self.selected_index = [1, sort_columns.index(sort_by)]
+        elif sort_by:
+            sort_columns = ("#", "Player Name", "trophies", "gain", "last_online ASC, player_name")
+            self.selected_index = [1, sort_columns.index(sort_by.replace('donations', 'trophies'))]
+        else:
             self.selected_index = []
 
     def get_readable(self, delta):
