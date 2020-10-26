e6a7c10ac885cf274cfa53d2699ea875a9989c04
mathsman5133 <mathsman5132@gmail.com>
2020-10-26 16:14:14 +1100

    set to 5:15

--- syncboards.py
+++ syncboards.py
@@ -482,9 +482,9 @@ class SyncBoards:
         log.info('running legend trophies')
         now = datetime.utcnow()
         if now.hour > 5:
-            tomorrow = (now + timedelta(days=1)).replace(hour=5, minute=12, second=0, microsecond=0)
+            tomorrow = (now + timedelta(days=1)).replace(hour=5, minute=15, second=0, microsecond=0)
         else:
-            tomorrow = now.replace(hour=5, minute=12, second=0, microsecond=0)
+            tomorrow = now.replace(hour=5, minute=15, second=0, microsecond=0)
 
         try:
             await asyncio.sleep((tomorrow - now).total_seconds())
