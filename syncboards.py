f9b5dcfa09c81a712097c3b5f28381dc22c87bb8
mathsman5133 <mathsman5132@gmail.com>
2022-09-18 20:58:35 +1000

    remove extra prints and move run_forevers around

--- syncboards.py
+++ syncboards.py
@@ -822,8 +822,8 @@ async def main():
 
         sb = SyncBoards(stateless_bot, start_loop=True, coc_client=client, pool=pool)
         await sb.on_init()
-        asyncio.get_event_loop().run_forever()
 
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(main())
+    asyncio.get_event_loop().run_forever()
