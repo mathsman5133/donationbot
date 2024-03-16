b8bf47f82650646bbc9d4fdd2dcbbf9dc5a23fa6
mathsman5133 <mathsman5132@gmail.com>
2024-03-17 10:48:08 +1100

    add distinct clause

--- cogs/boards.py
+++ cogs/boards.py
@@ -449,7 +449,7 @@ class BoardSetupMenu(discord.ui.View):
         return [BoardConfig(bot=self.cog.bot, record=row) for row in fetch]
 
     async def load_default_channel(self):
-        fetch = await self.cog.bot.pool.fetch("SELECT channel_id FROM boards WHERE guild_id=$1", self.guild.id)
+        fetch = await self.cog.bot.pool.fetch("SELECT DISTINCT channel_id FROM boards WHERE guild_id=$1", self.guild.id)
         channels = [self.guild.get_channel(row["channel_id"]) for row in fetch]
         channels = [c for c in channels if c]
         if not channels:
